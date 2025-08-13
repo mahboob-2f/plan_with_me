721141994b0f656b2d241816700e2eb08d790bcf
mahboob <mahboob6550@gmail.com>
2025-08-13 23:28:28 +0530

    restored all images

--- src/componets/Data.jsx
+++ src/componets/Data.jsx
@@ -3,49 +3,49 @@ const Data = [
         id:1,
         name:"agra",
         info: "The Taj Mahal, located in Agra, India, is one of the most famous and breathtaking monuments in the world. Built by the Mughal emperor Shah Jahan in the 17th century, it was made in memory of his beloved wife Mumtaz Mahal.",
-        image: "/tajmahal2.jpeg", //   here image of agra city 
+        image: "./tajmahal2.jpeg", //   here image of agra city 
         price: "10,000"
     },
     {
         id:2,
         name:"jaipur",
         info: "Jaipur, the capital city of Rajasthan, India, is famously known as the Pink City because of the distinct pink color of its historic buildings. Founded in 1727 by Maharaja Sawai Jai Singh II, Jaipur was one of India’s first planned cities, built with beautiful wide streets and organized architecture.",
-        image: "/jaipur.jpeg", //   here image of agra city 
+        image: "./jaipur.jpeg", //   here image of agra city 
         price: "23,000"
     },
     {
         id:3,
         name:"calcutta",
         info: "Kolkata, formerly known as Calcutta, is the cultural capital of India and the capital city of the state of West Bengal. Founded as a trading post by the British East India Company, Kolkata was once the capital of British India until 1911.",
-        image: "/calcutta.jpeg", //   here image of agra city 
+        image: "./calcutta.jpeg", //   here image of agra city 
         price: "10,000"
     },
     {
         id:4,
         name:"shimla",
         info: "Shimla, the charming hill station nestled in the Himalayas, is the capital city of Himachal Pradesh, India. Known as the Queen of Hills, Shimla was once the summer capital of British India because of its cool climate and scenic beauty.",
-        image: "/shimla.jpeg", //   here image of agra city 
+        image: "./shimla.jpeg", //   here image of agra city 
         price: "19,000"
     },
     {
         id:5,
         name:"manali",
         info: "Manali, located in the Kullu Valley of Himachal Pradesh, is one of India's most popular hill stations, known for its snow-capped mountains, lush valleys, and adventure sports. Set along the banks of the Beas River, Manali offers a perfect blend of natural beauty, thrill, and serenity.",
-        image: "/manali.jpeg", //   here image of agra city 
+        image: "./manali.jpeg", //   here image of agra city 
         price: "17,000"
     },
     {
         id:6,
         name:"Mumbai",
         info: "Mumbai, often called the City of Dreams, is the bustling financial, commercial, and entertainment capital of India. Located on the western coast along the Arabian Sea, Mumbai is the largest city in India and home to people from all across the country, making it a vibrant blend of cultures, languages, and traditions.",
-        image: "/mumbai.jpeg", //   here image of agra city 
+        image: "./mumbai.jpeg", //   here image of agra city 
         price: "20,000"
     },
     {
         id:7,
         name:"banglore",
         info: "Bangalore, also known as Bengaluru, is the capital city of Karnataka and one of India’s most vibrant and cosmopolitan cities. Nicknamed the Silicon Valley of India, Bangalore is the leading hub for information technology, startups, and innovation, attracting talent from across the country and the world.",
-        image: "/bb.jpg", //   here image of agra city 
+        image: "./bb.jpg", //   here image of agra city 
         price: "19,000"
     }
 ]
